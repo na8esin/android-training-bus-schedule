7385b34b0ecaca5b9df4d622306b2859d05674d4
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-26 14:10:35 +0900

    Define the DAO

---
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -0,0 +1,9 @@
+package com.example.busschedule.database.schedule
+
+@Dao
+interface ScheduleDao {
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
+}
