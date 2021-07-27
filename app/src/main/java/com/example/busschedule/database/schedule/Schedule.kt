b1aac884984cbd522dc58ecba0876eed8360a091
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-27 16:31:33 +0900

    Data class uses parentheses instead of curly braces

--- app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -6,10 +6,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Schedule {
+data class Schedule(
     @PrimaryKey
     val id: Int,
     @NonNull
     @ColumnInfo(name = "stop_name") val stopName: String,
     @NonNull @ColumnInfo(name = "arrival_time") val arrivalTime: Int
-}
+)
